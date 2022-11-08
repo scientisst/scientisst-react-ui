d45f01f92d9ef7ce063739c3808f27cc0c6a5c56
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-11-08 16:39:44 +0000

    remove default package export

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,11 @@ import TextButton from "../components/inputs/TextButton"
 
 const Page: NextPage = () => {
 	return (
-		<div>
-			<TextButton size="base">asdasd</TextButton>
+		<div className="flex w-full flex-col items-center justify-center p-8">
+			<div className="h-16" />
+			<TextButton size="base" className="font-primary">
+				Connect
+			</TextButton>
 		</div>
 	)
 }
