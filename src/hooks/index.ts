30dbc83576aa63aa1614c35f292f00bd0f2897ba
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-12-02 14:48:29 +0000

    initial lttb downsampling algorithm

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 import useDebounce from "./useDebounce"
+import useLiveD3Chart from "./useLiveD3Chart"
 import useMediaQuery from "./useMediaQuery"
 
-export { useDebounce, useMediaQuery }
+export { useDebounce, useMediaQuery, useLiveD3Chart }
