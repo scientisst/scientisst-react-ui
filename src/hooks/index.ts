56cdcbb22d462f8b0c6c99d31d6b81a2a3ff9344
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-12-06 15:38:46 +0000

    remove export of useLiveD3Chart hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 import useDebounce from "./useDebounce"
-import useLiveD3Chart from "./useLiveD3Chart"
 import useMediaQuery from "./useMediaQuery"
 
-export { useDebounce, useMediaQuery, useLiveD3Chart }
+export { useDebounce, useMediaQuery }
